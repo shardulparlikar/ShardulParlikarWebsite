416db549bfcabde98a58622e45c33e68ebc4024e
shardulparlikar <shardulparlikar@gmail.com>
2024-05-01 11:59:41 +0530

    Google Analytics Changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,31 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 
 export default function Document() {
   return (
     <Html lang="en" className="dark">
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-CWZ26WQ9XB"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-CWZ26WQ9XB');
-      </script>
         <title>Shardul Parlikar</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
           integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           referrerpolicy="no-referrer"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossorigin
+          crossOrigin
         ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@1,300&display=swap"
@@ -35,7 +28,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossorigin
+          crossOrigin
         ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Heebo:wght@100&family=Rubik:ital,wght@1,300&display=swap"
@@ -45,18 +38,25 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossorigin
+          crossOrigin
         ></link>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossorigin
+          crossOrigin
         ></link>
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
           rel="stylesheet"
         ></link>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-CWZ26WQ9XB" onLoad={()=> {
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-CWZ26WQ9XB');
+        }}/>
       </Head>
       <body className="dark">
         <Main />
